f1bf81a3c9273c433684eea6747e59f1a027a8d6
Joshua Hoffman <joshuahoffman@datamachines.io>
2020-10-04 21:46:42 -0500

    Try to parralelize

--- scripts/spinningup/ddpg.py
+++ scripts/spinningup/ddpg.py
@@ -4,10 +4,11 @@ import safety_gym
 import gym
 from spinup import td3_pytorch as ddpg
 from safe_rl.utils.run_utils import setup_logger_kwargs
+from safe_rl.utils.mpi_tools import mpi_fork
 from datetime import date
 
 
-def ddpg_wrapper(exp_name='', seed=10):
+def ddpg_wrapper(exp_name='', seed=10, cpu=1):
     if exp_name == '':
         today = date.today()
         date_fmt = today.strftime("%b_%d_%Y")
@@ -19,7 +20,13 @@ def ddpg_wrapper(exp_name='', seed=10):
     steps_per_epoch = 30000
     epochs = int(num_steps / steps_per_epoch)
     save_freq = 50
+
+    # Fork for parallelizing
+    mpi_fork(cpu)
+
+    # Prepare Logger
     logger_kwargs = setup_logger_kwargs(exp_name, seed)
+
     ddpg(lambda: gym.make('Safexp-PointButton1-v0'),
          steps_per_epoch=steps_per_epoch,
          epochs=epochs,
@@ -36,5 +43,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp_name', type=str, default='')
     parser.add_argument('--seed', type=int, default=10)
+    parser.add_argument('--cpu', type=int, default=1)
     args = parser.parse_args()
-    ddpg_wrapper(args.exp_name, args.seed)
+    ddpg_wrapper(args.exp_name, args.seed, args.cpu)
